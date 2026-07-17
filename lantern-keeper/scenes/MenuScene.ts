159b06ad76173c9506f64706359df42914c1e536
greentea524 <cpdawei@gmail.com>
2026-07-17 14:04:03 -0700

    feat: add About option to main menu with game backstory screen

--- lantern-keeper/scenes/MenuScene.ts
+++ lantern-keeper/scenes/MenuScene.ts
@@ -5,8 +5,10 @@ export class MenuScene extends Phaser.Scene {
   private selectedIndex = 0;
   private options: Phaser.GameObjects.Text[] = [];
   
-  private viewMode: 'menu' | 'controls' = 'menu';
+  private viewMode: 'menu' | 'controls' | 'about' = 'menu';
   private controlsText!: Phaser.GameObjects.Text;
+  private aboutContainer!: Phaser.GameObjects.Container;
+  private headerContainer!: Phaser.GameObjects.Container;
   private inputCooldownUntil = 0;
 
   constructor() {
@@ -18,7 +20,7 @@ export class MenuScene extends Phaser.Scene {
     this.cameras.main.setBackgroundColor('#0f1a12')
 
     // Title
-    this.add.text(GBC_WIDTH / 2, 20, 'LANTERN KEEPER', {
+    const titleText = this.add.text(GBC_WIDTH / 2, 18, 'LANTERN KEEPER', {
       fontFamily: '"Press Start 2P"',
       fontSize: '8px',
       color: '#e0f8cf',
@@ -26,36 +28,72 @@ export class MenuScene extends Phaser.Scene {
     }).setOrigin(0.5)
 
     // Player and Lantern Sneak Peek
-    this.add.sprite(GBC_WIDTH / 2 - 16, 50, 'player_idle').setScale(2);
-    this.add.sprite(GBC_WIDTH / 2 + 16, 50, 'lanternLit').setScale(2);
+    const playerPreview = this.add.sprite(GBC_WIDTH / 2 - 16, 44, 'player_idle').setScale(2);
+    const lanternPreview = this.add.sprite(GBC_WIDTH / 2 + 16, 44, 'lanternLit').setScale(2);
+
+    this.headerContainer = this.add.container(0, 0, [titleText, playerPreview, lanternPreview]);
 
     // Menu Options
-    const startText = this.add.text(GBC_WIDTH / 2, 85, 'Start Game', {
+    const startText = this.add.text(GBC_WIDTH / 2, 74, 'Start Game', {
       fontFamily: '"Press Start 2P"',
-      fontSize: '8px',
+      fontSize: '7px',
       color: '#86b06a',
       resolution: 1,
     }).setOrigin(0.5);
 
-    const controlsText = this.add.text(GBC_WIDTH / 2, 105, 'Controls', {
+    const controlsText = this.add.text(GBC_WIDTH / 2, 92, 'Controls', {
       fontFamily: '"Press Start 2P"',
-      fontSize: '8px',
+      fontSize: '7px',
       color: '#86b06a',
       resolution: 1,
     }).setOrigin(0.5);
 
-    this.options = [startText, controlsText];
+    const aboutOptionText = this.add.text(GBC_WIDTH / 2, 110, 'About', {
+      fontFamily: '"Press Start 2P"',
+      fontSize: '7px',
+      color: '#86b06a',
+      resolution: 1,
+    }).setOrigin(0.5);
+
+    this.options = [startText, controlsText, aboutOptionText];
     this.selectedIndex = 0;
     
     // Controls View
-    this.controlsText = this.add.text(GBC_WIDTH / 2, 90,
+    this.controlsText = this.add.text(GBC_WIDTH / 2, 85,
       'Arrows: Move & Jump\nX/B: Dash\n\nPress X to return', {
       fontFamily: '"Press Start 2P"',
-      fontSize: '8px',
+      fontSize: '7px',
       color: '#86b06a',
       align: 'center',
       resolution: 1,
     }).setOrigin(0.5).setVisible(false);
+
+    // About / Story View
+    const aboutTitle = this.add.text(GBC_WIDTH / 2, 20, 'STORY', {
+      fontFamily: '"Press Start 2P"',
+      fontSize: '8px',
+      color: '#e0f8cf',
+      resolution: 1,
+    }).setOrigin(0.5);
+
+    const storyText = this.add.text(GBC_WIDTH / 2, 68,
+      'An ancient darkness\nhas fallen upon\nthe realm.\n\nLight the ancient\nlanterns to restore\nyour powers & reach\nthe Heart Tree.', {
+      fontFamily: '"Press Start 2P"',
+      fontSize: '6px',
+      color: '#86b06a',
+      align: 'center',
+      lineSpacing: 3,
+      resolution: 1,
+    }).setOrigin(0.5);
+
+    const returnText = this.add.text(GBC_WIDTH / 2, 126, 'Press X to return', {
+      fontFamily: '"Press Start 2P"',
+      fontSize: '6px',
+      color: '#e0f8cf',
+      resolution: 1,
+    }).setOrigin(0.5);
+
+    this.aboutContainer = this.add.container(0, 0, [aboutTitle, storyText, returnText]).setVisible(false);
 
     this.input.keyboard!.on('keydown', this.handleKey, this);
     
@@ -67,6 +105,9 @@ export class MenuScene extends Phaser.Scene {
     });
     controlsText.setInteractive().on('pointerdown', () => {
       if (this.viewMode === 'menu') this.showControls();
+    });
+    aboutOptionText.setInteractive().on('pointerdown', () => {
+      if (this.viewMode === 'menu') this.showAbout();
     });
   }
   
@@ -83,19 +124,35 @@ export class MenuScene extends Phaser.Scene {
   }
 
   startGame() {
-    this.scene.start('play');
+    this.scene.start('play', { levelKey: 'level1', hasDoubleJump: false, hasDash: false, hasWallCling: false });
   }
   
   showControls() {
     this.viewMode = 'controls';
     this.options.forEach(opt => opt.setVisible(false));
+    this.headerContainer.setVisible(true);
     this.controlsText.setVisible(true);
   }
   
   hideControls() {
     this.viewMode = 'menu';
     this.options.forEach(opt => opt.setVisible(true));
+    this.headerContainer.setVisible(true);
     this.controlsText.setVisible(false);
+  }
+
+  showAbout() {
+    this.viewMode = 'about';
+    this.options.forEach(opt => opt.setVisible(false));
+    this.headerContainer.setVisible(false);
+    this.aboutContainer.setVisible(true);
+  }
+
+  hideAbout() {
+    this.viewMode = 'menu';
+    this.options.forEach(opt => opt.setVisible(true));
+    this.headerContainer.setVisible(true);
+    this.aboutContainer.setVisible(false);
   }
 
   handleKey(event: KeyboardEvent) {
@@ -104,6 +161,13 @@ export class MenuScene extends Phaser.Scene {
     if (this.viewMode === 'controls') {
       if (event.code === 'KeyX' || event.code === 'KeyZ' || event.code === 'Enter') {
         this.hideControls();
+      }
+      return;
+    }
+
+    if (this.viewMode === 'about') {
+      if (event.code === 'KeyX' || event.code === 'KeyZ' || event.code === 'Enter') {
+        this.hideAbout();
       }
       return;
     }
@@ -118,9 +182,11 @@ export class MenuScene extends Phaser.Scene {
 
     if (event.code === 'Enter' || event.code === 'KeyX' || event.code === 'KeyZ') {
       if (this.selectedIndex === 0) {
-        this.scene.start('play', { levelKey: 'level1', hasDoubleJump: false, hasDash: false, hasWallCling: false })
+        this.startGame();
       } else if (this.selectedIndex === 1) {
         this.showControls();
+      } else if (this.selectedIndex === 2) {
+        this.showAbout();
       }
     }
   }
